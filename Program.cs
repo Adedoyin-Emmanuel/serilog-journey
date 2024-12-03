31160ec9ac37df8dc6e55d27b1fd28e7c1f17776
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-12-03 01:27:19 +0100

    feat: init program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+class Program{
+
+    static public void Main(string[] args){
+        Console.WriteLine("Hello Serilog");
+    }
+}
