eb0687615e95bb58f0874ea8eb8b9fe70a09dc0c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-12-03 02:34:47 +0100

    feat: implemented logging to file

--- Program.cs
+++ Program.cs
@@ -1,22 +1,27 @@
 ﻿using System;
+using System.Net;
 using Serilog;
 using Serilog.Core;
+using Serilog.Formatting.Compact;
 
 
 class Program{
 
-    private static readonly ILogger _logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.Console().CreateLogger();
+    private static readonly string logPath = "./logs/log.json";
+    private static readonly ILogger _logger = new LoggerConfiguration().WriteTo.File(new CompactJsonFormatter(), logPath).CreateLogger();
 
     static public void Main(string[] args){
 
         try
         {
             _logger.Information("Something something");
+
+            throw new Exception("Something went wrong bruh");
         }
-        catch (System.Exception)
+        catch (System.Exception ex)
         {
-            
-            throw;
+            _logger.Error($"{ex}");
+
         }finally{
             _logger.Information("The program ended ");
         }
